a39e90ccf6899c3fba00c4c67db84b0f9a15d6e3
Amrit57 <fenpadorje7@gmail.com>
2023-05-24 14:56:25 +0545

    Created MainPage and added Navbar and bg-image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
 import _ from 'lodash';
+import './style.css';
 
-console.log('hello from amrit')
-console.log('hero honda')
+// import menuPage from './menu.js'
+// import contactPage from './contact.js'
+function pageLoader() {
+
+    const main = document.createElement('div');
+    const nav = document.createElement('div');
+    const content = document.createElement('div');
+    main.setAttribute('id', 'main');
+    nav.setAttribute('id', 'nav');
+    content.setAttribute('id', 'content');
+    const home = document.createElement('li');
+    const menu = document.createElement('li');
+    const contact = document.createElement('li');
+    home.textContent = 'Home';
+    menu.textContent = 'Menu';
+    contact.textContent = 'Contact';
+    nav.appendChild(home);
+    nav.appendChild(menu);
+    nav.appendChild(contact);
+    
+    function homePage() {
+        const text = document.createElement('div')
+        text.textnav = 'this is homepage'
+        text.classList.add('blue');
+        return text
+
+    }
+    home.appendChild(homePage())
+
+
+    main.appendChild(nav)
+    main.appendChild(content);
+    return main;
+}
+document.body.appendChild(pageLoader())

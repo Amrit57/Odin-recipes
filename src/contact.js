9b11f256497748a1ef9fdfbbb1798e1708c6a734
Amrit57 <fenpadorje7@gmail.com>
2023-05-25 10:55:53 +0545

    Created Form for reservation booking

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,46 @@
 
-export default function homeRender() {
-    const text = document.createElement('li')
-    text.textContent = 'Contact page'
-    text.classList.add('blue');
-    return text
+export default function contactRender() {
+    const content = document.createElement('div')
+    content.setAttribute('id', 'contact-wrapper')
+
+    const header = document.createElement('h1')
+    header.textContent = 'Reservation Booking'
+
+    const nameDiv = document.createElement('div')
+    nameDiv.textContent = 'Name';
+    const name = document.createElement('input')
+    nameDiv.appendChild(name)
+
+    const numberDiv = document.createElement('div')
+    numberDiv.textContent = 'Phone Number';
+    const number = document.createElement('input')
+    number.setAttribute('type', 'number')
+    numberDiv.appendChild(number)
+
+
+
+    const reservationDiv = document.createElement('div')
+    reservationDiv.textContent = 'Reservation'
+    const reservation = document.createElement('input')
+    reservation.setAttribute('type', 'number')
+    reservationDiv.appendChild(reservation)
+
+
+    const timeDiv = document.createElement('div')
+    timeDiv.textContent = 'Date of Reservation'
+    const time = document.createElement('input')
+    time.setAttribute('type', 'date')
+    timeDiv.appendChild(time);
+
+    const submitBtn = document.createElement('button')
+    submitBtn.textContent = 'Submit';
+
+    content.appendChild(header)
+    content.appendChild(nameDiv)
+    content.appendChild(numberDiv)
+    content.appendChild(reservationDiv)
+    content.appendChild(timeDiv)
+    content.appendChild(submitBtn)
+
+    return content
 }
